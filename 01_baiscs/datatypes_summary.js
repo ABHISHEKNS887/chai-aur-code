5ca1b8e1ed5350eac891a005c500436d77ae89cc
Abhishek N S <73115881+ABHISHEKNS887@users.noreply.github.com>
2023-11-29 11:15:50 +0000

    stack and heap

--- 01_baiscs/datatypes_summary.js
+++ 01_baiscs/datatypes_summary.js
@@ -11,3 +11,20 @@ const bigNumber = 123123123123n // BigInt
 // Array, Object, Functions
 
 // Note: typeof null = object
+
+// ************************************
+
+// Memory
+
+// Stack (Primitive) and Heap(Non Primitive)
+
+// In stack if we change the value of duplicated variable, the value of original variable remains same. 
+//In case of Heap memory this not happens.
+
+let a = 'abhishek'
+let b = a
+b = 'manoj'
+
+let c = {'name': 'abhi'}
+let d = c
+d.name = 'manoj'
